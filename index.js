498f6140605de5e156920243aa2010c00373b38d
gel1ler <57824633+gel1ler@users.noreply.github.com>
2022-07-08 00:43:23 +0300

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.get('/find', async (req, res) => {
     let name = req.query.name
     name = decodeURI(name)
     try {
-        
         const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=${name}`, {
             headers: {
                 'X-API-KEY': '4b403f23-526e-4a28-8173-2c724b350b9a',
@@ -22,16 +21,27 @@ app.get('/find', async (req, res) => {
         })
         const list = await response.json()
 
+        console.log(list.films)
 
         const result = []
 
         list.films.forEach(i => {
             result.push({
+                id: i.filmId,
                 name: i.nameRu,
                 rating: i.rating,
                 poster: i.posterUrl,
+                description: i.description
             })
         })
+
+        // const result = [
+        //     { name: 'Форсаж', rating: 8, poster: 'https://i.pinimg.com/originals/5f/17/5f/5f175fb6572aff90ca340b715c06e579.jpg', description:'Пристегните ремни — гонка продолжается. Гавана, Берлин, Нью-Йорк — для самой крутой команды в мире нет ничего невозможного, пока они вместе. Но когда на их пути окажется одна из самых красивых женщин на планете и по совместительству королева киберпреступности, дороги друзей разойдутся.' },
+        //     { name: 'Форсаж-2', rating: 8, poster: 'https://i.pinimg.com/originals/5f/17/5f/5f175fb6572aff90ca340b715c06e579.jpg', description:'Пристегните ремни — гонка продолжается. Гавана, Берлин, Нью-Йорк — для самой крутой команды в мире нет ничего невозможного, пока они вместе. Но когда на их пути окажется одна из самых красивых женщин на планете и по совместительству королева киберпреступности, дороги друзей разойдутся.' },
+        //     { name: 'Форсаж-3', rating: 2, poster: 'https://i.pinimg.com/originals/5f/17/5f/5f175fb6572aff90ca340b715c06e579.jpg', description:'Пристегните ремни — гонка продолжается. Гавана, Берлин, Нью-Йорк — для самой крутой команды в мире нет ничего невозможного, пока они вместе. Но когда на их пути окажется одна из самых красивых женщин на планете и по совместительству королева киберпреступности, дороги друзей разойдутся.' },
+        //     { name: 'Форсаж-4', rating: 7, poster: 'https://i.pinimg.com/originals/5f/17/5f/5f175fb6572aff90ca340b715c06e579.jpg', description:'Пристегните ремни — гонка продолжается. Гавана, Берлин, Нью-Йорк — для самой крутой команды в мире нет ничего невозможного, пока они вместе. Но когда на их пути окажется одна из самых красивых женщин на планете и по совместительству королева киберпреступности, дороги друзей разойдутся.' },
+        // ]
+
         if (result.length > 0) {
             return res.json(result)
         }
@@ -46,6 +56,31 @@ app.get('/find', async (req, res) => {
     }
 })
 
+app.get('/film', async (req, res) => {
+    let id = req.query.id
+    try {
+        const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`, {
+            headers: {
+                'X-API-KEY': '4b403f23-526e-4a28-8173-2c724b350b9a',
+                accept: 'application/json'
+            }
+        })
+        const film = await response.json()
+        console.log(film)
+
+        const result = [{
+            name: film.nameRu,
+            description: film.description,
+            rating: film.ratingImdb,
+            date: film.year
+        }]
+
+        return res.json(result)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
 
 async function start() {
     try {
